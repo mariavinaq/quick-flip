56485e9e17f6f0024daa88f37a72fbcb6cf042d5
Vina Quiambao <mariavinaq@gmail.com>
2023-08-25 00:36:55 -0400

    Add improvements

--- js/main.js
+++ js/main.js
@@ -1,69 +1,62 @@
 import * as THREE from 'three';
 
-// SCENE, CAMERA, RENDERER
+// Initialize scene, camera, and renderer
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 100 );
+const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 100);
+const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
 
-const renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-// DIRECTIONAL LIGHT
+// Directional light
 const light = new THREE.DirectionalLight();
 light.position.set(-0.25, 0.5, 1);
 scene.add(light);
 
-// COIN
-const texture = new THREE.TextureLoader()
-const urls = ["plain-NormalMap.png", "head-NormalMap.png", "tail-NormalMap.png"]
-const geometry = new THREE.CylinderGeometry( 1, 1, 0.12, 1000 );
-const materials = urls.map(url => {
-    return new THREE.MeshStandardMaterial({
+// Coin
+const texture = new THREE.TextureLoader();
+const urls = ["plain-NormalMap.png", "head-NormalMap.png", "tail-NormalMap.png"];
+const geometry = new THREE.CylinderGeometry(1, 1, 0.12, 1000);
+const materials = urls.map(url => new THREE.MeshStandardMaterial({
         normalMap: texture.load(url),
         metalness: 0.8,
         roughness: 0.5,
         color: 0xffd300,
-    })
-})
-const coin = new THREE.Mesh( geometry, materials );
-coin.rotation.x = 0.5 * Math.PI / 2;
-scene.add( coin );
+}));
+
+const coin = new THREE.Mesh(geometry, materials);
+coin.rotation.x = 0.5 * Math.PI / 2; // to start with heads at a slight angle
+scene.add(coin);
 
 camera.position.z = 5;
 
-    // COIN FLIP/TILT
-    let flipSpeed = 0.02;
-    let tiltSpeed = 0;
-    let tiltDirection = 0; 
-
-// RENDER SCENE
+// Coin animation and render function
+let flipSpeed = 0.02;
+let tiltSpeed = 0;
+let tiltDirection = 0; 
 
 function animate() {
-	requestAnimationFrame( animate );
+	requestAnimationFrame(animate);
     coin.rotation.x += flipSpeed;
     coin.rotation.z += tiltSpeed * tiltDirection;
     if (coin.rotation.z > 0.35 || coin.rotation.z < -0.35) {
         tiltDirection *= -1;
     }
-	renderer.render( scene, camera );
+	renderer.render(scene, camera);
 }
 animate();
 
-// INSTRUCTIONS
+// Click handlers
+const instructions = document.getElementById('tap');
+let flipping = false;
 
-let instructions = document.getElementById('tap')
-
-// CLICK FLIP
-
-let flipping = false
-
-document.addEventListener('click', function() {
+document.addEventListener('click', () => {
     if (flipping === false) {
         flipping = true;
+        flipSpeed = 0.35;
         tiltSpeed = 0;
         coin.rotation.z = 0;
-        flipSpeed = 0.35
-        instructions.textContent = instructions.textContent.replace('flip', 'stop')
+        instructions.textContent = 'tap screen to stop';
     } else {
         flipping = false;
         if (Math.random() < 0.5) {
@@ -76,25 +69,26 @@ document.addEventListener('click', function() {
         flipSpeed = 0;
         tiltSpeed = 0.0075;
         tiltDirection = 1;
-        instructions.textContent = instructions.textContent.replace('stop', 'flip')
+        instructions.textContent = 'tap screen to flip';
     }
-})
+});
 
-// INFO
+// Info handlers
 
-let open = false
+let open = false;
+const info = document.getElementById('info');
 
-document.querySelector('i').addEventListener('click', function(event) {
+document.querySelector('i').addEventListener('click', event => {
     event.stopPropagation();
     if (open === false) {
-        document.getElementById('info').style.visibility = 'visible';
-        open = true
+        info.style.visibility = 'visible';
+        open = true;
     } else {
-        document.getElementById('info').style.visibility = 'hidden';
-        open = false
+        info.style.visibility = 'hidden';
+        open = false;
     }
-})
+});
 
-document.querySelector('a').addEventListener('click', function(event) {
+document.querySelector('a').addEventListener('click', event => {
     event.stopPropagation();
-})
+});
